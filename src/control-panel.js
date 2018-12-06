e74ec55afe1f2131983418c794f88861b3e11a1a
Pha Nguyen <sephanguyen@gmail.com>
2018-12-06 15:21:10 +0700

    implement store

--- src/control-panel.js
+++ src/control-panel.js
@@ -1,4 +1,4 @@
-import { Dispatcher } from './flux';
+import { Dispatcher, Store } from './flux';
 
 const controlPanelDispatcher = new Dispatcher();
 document
@@ -13,6 +13,30 @@ document.forms.fontSizeForm.fontSize.forEach(element => {
   element.addEventListener('change', ({ target }) => {
     controlPanelDispatcher.dispatch(`TODO_FONTUPDATEACTION`);
   });
+});
+
+class UserPrefsStore extends Store {
+  getInitialState() {
+    return {
+      userName: 'Jim',
+      fontSize: 'small'
+    };
+  }
+
+  __onDispatch(action) {
+    console.log('Store received dispatch', action);
+    this.__emitChange();
+  }
+
+  getUserPrefences() {
+    return this.__state;
+  }
+}
+
+const userPrefsStore = new UserPrefsStore(controlPanelDispatcher);
+
+userPrefsStore.addListener(state => {
+  console.info(`The current state is...`, state);
 });
 
 controlPanelDispatcher.register(action => {
