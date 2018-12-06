035d2a882fe5aa3d7fe093153a3e27043412f8c0
Pha Nguyen <sephanguyen@gmail.com>
2018-12-06 16:53:03 +0700

    implement Actions

--- src/control-panel.js
+++ src/control-panel.js
@@ -1,30 +1,59 @@
 import { Dispatcher, Store } from './flux';
 
 const controlPanelDispatcher = new Dispatcher();
+
+const UPDATE_USERNAME = `UPDATE_USERNAME`;
+const UPDATE_FONT_SIZE_PREFERENCE = `UPDATE_FONT_SIZE_PREFERENCE`;
+
+const userNameUpdateAction = name => {
+  return {
+    type: UPDATE_USERNAME,
+    value: name
+  };
+};
+
+const fontSizePreferenceUpdateAction = size => {
+  return {
+    type: UPDATE_FONT_SIZE_PREFERENCE,
+    value: size
+  };
+};
+
 document
   .getElementById(`userNameInput`)
   .addEventListener(`input`, ({ target }) => {
     const name = target.value;
     console.log('Dispatching....', name);
-    controlPanelDispatcher.dispatch(`TODO_NAMEINPUTACTION`);
+    controlPanelDispatcher.dispatch(userNameUpdateAction(name));
   });
 
 document.forms.fontSizeForm.fontSize.forEach(element => {
   element.addEventListener('change', ({ target }) => {
-    controlPanelDispatcher.dispatch(`TODO_FONTUPDATEACTION`);
+    controlPanelDispatcher.dispatch(
+      fontSizePreferenceUpdateAction(target.value)
+    );
   });
 });
 
 class UserPrefsStore extends Store {
   getInitialState() {
-    return {
-      userName: 'Jim',
-      fontSize: 'small'
-    };
+    return localStorage[`preferences`]
+      ? JSON.parse(localStorage[`preferences`])
+      : {
+          userName: 'Jim',
+          fontSize: 'small'
+        };
   }
 
   __onDispatch(action) {
-    console.log('Store received dispatch', action);
+    switch (action.type) {
+      case UPDATE_USERNAME:
+        this.__state.userName = action.value;
+        break;
+      case UPDATE_FONT_SIZE_PREFERENCE:
+        this.__state.fontSize = action.value;
+        break;
+    }
     this.__emitChange();
   }
 
@@ -37,8 +66,18 @@ const userPrefsStore = new UserPrefsStore(controlPanelDispatcher);
 
 userPrefsStore.addListener(state => {
   console.info(`The current state is...`, state);
+  render(state);
+  localStorage[`preferences`] = JSON.stringify(state);
 });
 
-controlPanelDispatcher.register(action => {
-  console.info('Received action....', action);
-});
+const render = ({ userName, fontSize }) => {
+  document.getElementById('userName').innerText = userName;
+  document.getElementsByClassName('container')[0].style.fontSize =
+    fontSize === 'small' ? '16px' : '24px';
+  document.forms.fontSizeForm.fontSize.value = fontSize;
+};
+
+// controlPanelDispatcher.register(action => {
+//   console.info('Received action....', action);
+// });
+render(userPrefsStore.getUserPrefences());
