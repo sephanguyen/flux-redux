355d40227a3a537b69014c7f542b6da4afa2c724
Pha Nguyen <sephanguyen@gmail.com>
2018-12-07 15:43:01 +0700

    implement reducer

--- src/message-board.js
+++ src/message-board.js
@@ -1,4 +1,11 @@
-import { createStore } from 'redux';
+import { createStore, combineReducers } from 'redux';
+
+export const ONLINE = `ONLINE`;
+export const AWAY = `AWAY`;
+export const BUSY = `BUSY`;
+export const OFFLINE = `OFFLINE`;
+
+export const UPDATE_STATUS = `UPDATE_STATUS`;
 
 const defaultState = {
   messages: [
@@ -13,12 +20,18 @@ const defaultState = {
       content: `I dont know if the new feature....`
     }
   ],
-  userStatus: `ONLINE`
+  userStatus: ONLINE
 };
 
-const store = createStore((state = defaultState) => {
+const reducer = (state = defaultState, { type, value }) => {
+  switch (type) {
+    case UPDATE_STATUS:
+      return { ...state, userStatus: value };
+  }
   return state;
-});
+};
+
+const store = createStore(reducer);
 
 const render = () => {
   const { messages, userStatus } = store.getState();
@@ -26,6 +39,20 @@ const render = () => {
     .sort((a, b) => b.date - a.date)
     .map(message => `<div>${message.postedBy} : ${message.content}</div>`)
     .join('');
+
+  document.forms.newMessage.fields.disabled = userStatus === OFFLINE;
 };
 
+const statusUpdateAction = value => {
+  return {
+    type: UPDATE_STATUS,
+    value
+  };
+};
+
+document.forms.selectStatus.status.addEventListener('change', e => {
+  store.dispatch(statusUpdateAction(e.target.value));
+});
+
 render();
+store.subscribe(render);
