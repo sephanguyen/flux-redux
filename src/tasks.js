d7e407e2d0133c882736a687e8a562a0b668bb13
Pha Nguyen <sephanguyen@gmail.com>
2018-12-07 11:41:25 +0700

    add reducer

--- src/tasks.js
+++ src/tasks.js
@@ -55,6 +55,34 @@ class TaskStore extends ReduceStore {
 
   reduce(state, action) {
     console.log('Reducing...', state, action);
+    let newState;
+    switch (action.type) {
+      case CREATE_TASK:
+        newState = { ...state, tasks: [...state.tasks] };
+        newState.tasks.push({
+          id: id(),
+          content: action.value,
+          complete: false
+        });
+        return newState;
+      case COMPLETE_TASK:
+        newState = { ...state, tasks: [...state.tasks] };
+        const affectedElementIndex = newState.tasks.findIndex(
+          t => t.id === action.id
+        );
+        newState.tasks[affectedElementIndex] = {
+          ...state.tasks[affectedElementIndex],
+          complete: action.value
+        };
+        return newState;
+      case SHOW_TASK:
+        newState = {
+          ...state,
+          tasks: [...state.tasks],
+          showComplete: action.value
+        };
+        return newState;
+    }
     return state;
   }
   getState() {
@@ -77,7 +105,20 @@ const render = () => {
     .map(TaskComponent)
     .join('');
   tasksSection.innerHTML = rendered;
+
+  document.getElementsByName(`taskCompleteCheck`).forEach(element => {
+    element.addEventListener(`change`, e => {
+      const id = e.target.attributes['data-taskid'].value;
+      const checked = e.target.checked;
+      taskDispatcher.dispatch(compleTaskAction(id, checked));
+    });
+  });
 };
+
+document.forms.undo.addEventListener(`submit`, e => {
+  e.preventDefault();
+  taskStore.revertLastState();
+});
 
 document.forms.newTask.addEventListener(`submit`, e => {
   e.preventDefault();
@@ -95,5 +136,7 @@ document
     taskDispatcher.dispatch(showTasksAction(showComplete));
   });
 
-taskDispatcher.dispatch(`TEST_DISPATCH`);
+taskStore.addListener(() => {
+  render();
+});
 render();
